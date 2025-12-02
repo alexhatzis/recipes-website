e2829c4c0c1bb72eba04a96e63a53ef9cc1ef616
alexhatzis <72629805+alexhatzis@users.noreply.github.com>
2025-12-01 16:36:53 -0800

    Update convert-recipes.sh

--- convert-recipes.sh
+++ convert-recipes.sh
@@ -48,10 +48,11 @@ mkdir -p "$OUTPUT_DIR"
 cat > "$INDEX_FILE" <<EOF
 <!DOCTYPE html>
 <html>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="./styles.css">
 <head>
 <meta charset="UTF-8">
 <title>Recipe Index</title>
-<link rel="stylesheet" href="./styles.css">
 </head>
 <body>
 <h1>Recipe Index</h1>
